671a844c8ddc5783b62fec8ccad5b1f057b7ba53
simao <simaotojal@ua.pt>
2024-12-24 11:03:16 +0000

    Otimização uando o graph não tem edges dá logo return

--- Trabalho_2_Ficheiros_para_os_ESTUDANTES/GraphTransitiveClosure.c
+++ Trabalho_2_Ficheiros_para_os_ESTUDANTES/GraphTransitiveClosure.c
@@ -26,12 +26,41 @@
 // Compute the transitive closure of a directed graph
 // Return the computed transitive closure as a directed graph
 // Use the Bellman-Ford algorithm
-Graph* GraphComputeTransitiveClosure(Graph* g) {
+Graph *GraphComputeTransitiveClosure(Graph *g)
+{
   assert(g != NULL);
   assert(GraphIsDigraph(g));
   assert(GraphIsWeighted(g) == 0);
 
   // COMPLETE THE CODE
+  unsigned int numVertices = GraphGetNumVertices(g);
+  unsigned int numEdges = GraphGetNumEdges(g);
+  Graph *res = GraphCreate(numVertices, GraphIsDigraph(g), GraphIsWeighted(g));
 
-  return NULL;
+  // if the graph doesn have any edge return and avoid wasting time
+  if (numEdges == 0)
+  {
+    return res;
+  }
+
+  // for every vertice
+  for (unsigned int startVertex = 0; startVertex < numVertices; startVertex++)
+  {
+    // create a graph using the bellman algorith for that starting vertx
+    GraphBellmanFordAlg *BF_result = GraphBellmanFordAlgExecute(g, startVertex);
+    for (unsigned int v = 0; v < numVertices; v++)
+    {
+      // check the distance of that vertice
+      int value = GraphBellmanFordAlgDistance(BF_result, v);
+      // check if the distance is grater than 0
+      if (value > 0)
+      {
+        // add the vertice
+        GraphAddEdge(g, startVertex, v);
+      }
+    }
+    GraphBellmanFordAlgDestroy(&BF_result);
+  }
+
+  return res;
 }
